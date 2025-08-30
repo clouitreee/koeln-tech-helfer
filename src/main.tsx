3f123fbf014f371813f82a73293077335e4f687c
clouitreee <proj.numerate352@passmail.com>
2025-08-30 09:43:47 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,5 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Impressum from "./pages/impressum";
-import Datenschutz from "./pages/datenschutz";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const rootEl = document.getElementById("root")!;
-createRoot(rootEl).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/impressum" element={<Impressum />} />
-        <Route path="/datenschutz" element={<Datenschutz />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
