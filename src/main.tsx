630daf3cc5937c6291a4f50913a441ef5e5fc0f1
Martin Lache <info@claveon.de>
2025-08-30 10:40:42 +0200

    Configurar React Router para rutas /impressum y /datenschutz

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Impressum from "./pages/impressum";
+import Datenschutz from "./pages/datenschutz";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/impressum" element={<Impressum />} />
+        <Route path="/datenschutz" element={<Datenschutz />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
